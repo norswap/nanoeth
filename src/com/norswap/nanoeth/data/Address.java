4189fb7ac1ac1cab8a3536deea94af982132ee02
Nicolas "Norswap" Laurent <norswap@gmail.com>
2021-07-25 21:49:40 +0200

    add utility to the Address class

--- src/com/norswap/nanoeth/data/Address.java
+++ src/com/norswap/nanoeth/data/Address.java
@@ -2,6 +2,9 @@ package com.norswap.nanoeth.data;
 
 import com.norswap.nanoeth.annotations.Retained;
 import com.norswap.nanoeth.annotations.Wrapper;
+import com.norswap.nanoeth.utils.Assert;
+import com.norswap.nanoeth.utils.ByteUtils;
+import java.util.Arrays;
 
 /**
  * Represents a 160-bit (20 bytes) Ethereum address.
@@ -19,7 +22,11 @@ public final class Address {
 
     // ---------------------------------------------------------------------------------------------
 
-    private final byte[] address;
+    /**
+     * The big-endian representation of the address. Usually 20-bytes long, zero-length if {@link
+     * #EMPTY}.
+     */
+    public final byte[] bytes;
 
     // ---------------------------------------------------------------------------------------------
 
@@ -27,24 +34,40 @@ public final class Address {
      * Constructor for the {@link #EMPTY} address.
      */
     private Address() {
-        this.address = new byte[0];
+        this.bytes = new byte[0];
     }
 
     // ---------------------------------------------------------------------------------------------
 
-    public Address (@Retained byte[] address) {
-        assert address.length == 20;
-        this.address = address;
+    public Address (@Retained byte[] bytes) {
+        Assert.that(bytes.length == 20, "byte array for address is not 20 bytes long");
+        this.bytes = bytes;
     }
 
     // ---------------------------------------------------------------------------------------------
 
     /**
-     * Returns the big-endian representation of the address. Usually 20-bytes long, zero-length
-     * if {@link #EMPTY}.
+     * Creates a natural from a hex-string (e.g. "0x95ad61b0a150d79219dcf64e1e6cc01f0b64c4ce").
+     *
+     * <p>If the post-0x part of the hex string is not 40 characters long, the address will be
+     * padded with zeroes at the start so that it is 20 bytes long.
      */
-    public byte[] address() {
-        return address;
+    public Address (String hexString) {
+        this(ByteUtils.hexStringToBytes(hexString, 20));
+    }
+
+    // ---------------------------------------------------------------------------------------------
+
+    @Override public boolean equals (Object o) {
+        return this == o || o instanceof Address && Arrays.equals(bytes, ((Address) o).bytes);
+    }
+
+    @Override public int hashCode () {
+        return Arrays.hashCode(bytes);
+    }
+
+    @Override public String toString() {
+        return ByteUtils.bytesToHexString(bytes);
     }
 
     // ---------------------------------------------------------------------------------------------
