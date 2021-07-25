a582c894b21b29ca5a9d9db13fe131652754b1b0
Nicolas "Norswap" Laurent <norswap@gmail.com>
2021-07-25 22:41:26 +0200

    remove unused function

--- src/com/norswap/nanoeth/utils/Crypto.java
+++ src/com/norswap/nanoeth/utils/Crypto.java
@@ -1,10 +1,7 @@
 package com.norswap.nanoeth.utils;
 
 import org.bouncycastle.jce.provider.BouncyCastleProvider;
-import org.bouncycastle.math.ec.ECPoint;
-import java.math.BigInteger;
 import java.security.Security;
-import java.util.Arrays;
 
 /**
  * Cryptography-related utilities & constants.
@@ -20,21 +17,6 @@ public final class Crypto {
         // even if it comes after the static block!
         Security.addProvider(new BouncyCastleProvider());
         BOUNCY_CASTLE_KEY_PROVIDER = "BC";
-    }
-
-    // ---------------------------------------------------------------------------------------------
-
-    // TODO unused - useful? (inspired from web3j which stores the public key as a BigInteger)
-
-    /**
-     * Converts the elliptic curve point into a big integer by getting the uncompressed byte array
-     * encoding of the point (as per SEC1 §2.3.3) and creating a positive number from that
-     * representation.
-     */
-    public static BigInteger asInteger (ECPoint point) {
-        byte[] bytes = point.getEncoded(false);
-        // we strip the first byte which signifies that the representation is uncompressed
-        return new BigInteger(1, Arrays.copyOfRange(bytes, 1, bytes.length));
     }
 
     // ---------------------------------------------------------------------------------------------
